8f1c0cbd390d70f110be60d98a551af4e9ec35cc
grjensen <grjensen@iu.edu>
2025-10-24 16:40:19 -0400

    add average fn

--- rand.h
+++ rand.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <random>
+#include <functional>
 
 std::uniform_real_distribution<> dis(0.0, 1.0);
 std::random_device rd;
@@ -8,4 +9,12 @@ std::mt19937 gen(rd());
 
 float random_probability() {
   return dis(gen);
+}
+
+float average(std::function<float(void)> f, uint n) {
+  float total = 0.f;
+  for (uint i = 0; i < n; i++) {
+    total += f();
+  }
+  return total / n;
 }
