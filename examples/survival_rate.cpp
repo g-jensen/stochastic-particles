c2997381d9e60c247521fd25fae6f3aeb6aa2392
grjensen <grjensen@iu.edu>
2025-10-24 16:49:58 -0400

    add survival_rate.cpp

---
+++ examples/survival_rate.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "../distribution.h"
+#include "../simulation.h"
+
+uint surivial_count(simulation& sim, float length) {
+  uint count = 0;
+  for (particle& p : sim.particles) {
+    if (p.velocity*p.lifespan >= length) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main() {
+  float mean_lifespan = 1.f;
+  float length = 1.f;
+  auto death_distribution_fn = [&](float rand) {return exponential_distribution(rand,mean_lifespan);};
+  int particle_count = 1000;
+
+  auto simulate_fn = [&](float velocity) {
+    return [&, velocity]() {
+      simulation sim(death_distribution_fn,particle_count,velocity);
+      return (float)surivial_count(sim,length) / sim.particles.size();
+    };
+  };
+
+  std::cout << "Survival Rates with Velocity v:" << std::endl;
+  std::cout << "v=0:    " << average(simulate_fn(0.f),1000) << std::endl;
+  std::cout << "v=0.25: " << average(simulate_fn(0.25f),1000) << std::endl;
+  std::cout << "v=0.5:  " << average(simulate_fn(0.5f),1000) << std::endl;
+  std::cout << "v=1:    " << average(simulate_fn(1.f),1000) << std::endl;
+  std::cout << "v=2:    " << average(simulate_fn(2.f),1000) << std::endl;
+}
