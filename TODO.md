7874f53b8a3d73a5abef0178d252a42fba301807
greg <greg.micah.jensen@gmail.com>
2025-11-03 00:04:40 -0500

    exponential_distribution asserts params are valid

--- TODO.md
+++ TODO.md
@@ -1,3 +1,3 @@
-- [ ] calling death_distribution should assert the params are valid
+- [x] calling death_distribution should assert the params are valid
 - [x] make survival_rate.cpp cout ore useful
 - [ ] move .h impls to proper .cpp file and create Makefile
--- distribution.h
+++ distribution.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <cmath>
+#include <cassert>
 
 // Inverse of f(t) = 1 - e^{-t/mean}. Note: f'(t)=1/mean * e^{-t/mean} which is what is meant by exponential.
 float exponential_distribution(float rand, float mean) {
+  assert(0 <= rand && rand <= 1);
   return -mean * log(1-rand);
 }
