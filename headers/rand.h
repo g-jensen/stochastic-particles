e335fcc809d9e4b41343593b5ca073c7e2356fd0
greg <greg.micah.jensen@gmail.com>
2026-01-16 19:26:29 -0500

    better naming for seed

--- headers/rand.h
+++ headers/rand.h
@@ -3,6 +3,6 @@
 #include <random>
 #include <functional>
 
-void seed_random(int* seed);
+void seed_random(int* seed_ptr);
 float random_probability();
 float average(std::function<float(void)> f, uint n);
--- src/rand.cpp
+++ src/rand.cpp
@@ -4,11 +4,11 @@ std::uniform_real_distribution<> dis(0.0, 1.0);
 std::random_device rd;
 std::mt19937 gen(rd());
 
-void seed_random(int* seed) {
-  if (*seed < 0) {
-    *seed = rd();
+void seed_random(int* seed_ptr) {
+  if (*seed_ptr < 0) {
+    *seed_ptr = rd();
   }
-  gen.seed(*seed);
+  gen.seed(*seed_ptr);
 }
 
 float random_probability() {
