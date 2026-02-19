808edb88972cbe45f565f8b2aff7bd934703a395
greg <greg.micah.jensen@gmail.com>
2026-02-18 21:07:05 -0500

    refactor specs

--- rust/src/static/simulation/test.rs
+++ rust/src/static/simulation/test.rs
@@ -1,44 +1,44 @@
 use super::*;
 use fraction::Fraction;
 
-fn f(n: u64, d: u64) -> Fraction {
+fn frac(n: u64, d: u64) -> Fraction {
     Fraction::new(n, d)
 }
 
 #[test]
 fn should_pass_lap_zero_phase_within_on_ratio() {
     assert_eq!(
-        should_pass_lap(&Fraction::new(0u64, 1u64), 0, &f(1, 1), &f(1, 2)),
+        should_pass_lap(&frac(0u64, 1u64), 0, &frac(1, 1), &frac(1, 2)),
         true
     );
 }
 
 #[test]
 fn should_pass_lap_phase_beyond_on_ratio() {
-    assert_eq!(should_pass_lap(&f(3, 5), 0, &f(1, 1), &f(1, 2)), false);
+    assert_eq!(should_pass_lap(&frac(3, 5), 0, &frac(1, 1), &frac(1, 2)), false);
 }
 
 #[test]
 fn should_pass_lap_count_pushes_phase_beyond_on_ratio() {
-    assert_eq!(should_pass_lap(&f(1, 5), 1, &f(1, 2), &f(1, 2)), false);
+    assert_eq!(should_pass_lap(&frac(1, 5), 1, &frac(1, 2), &frac(1, 2)), false);
 }
 
 #[test]
 fn should_pass_lap_wraps_phase_past_one() {
-    assert_eq!(should_pass_lap(&f(4, 5), 1, &f(1, 2), &f(1, 2)), true);
+    assert_eq!(should_pass_lap(&frac(4, 5), 1, &frac(1, 2), &frac(1, 2)), true);
 }
 
 #[test]
 fn max_laps_counts_consecutive_passing_laps() {
-    assert_eq!(max_laps(&Fraction::new(0u64, 1u64), &f(3, 10), &f(1, 2)), 1);
+    assert_eq!(max_laps(&frac(0u64, 1u64), &frac(3, 10), &frac(1, 2)), 1);
 }
 
 #[test]
 fn max_laps_zero_when_phase_beyond_on_ratio() {
-    assert_eq!(max_laps(&f(3, 5), &f(3, 10), &f(1, 2)), 0);
+    assert_eq!(max_laps(&frac(3, 5), &frac(3, 10), &frac(1, 2)), 0);
 }
 
 #[test]
 fn max_laps_offsets_by_phase() {
-    assert_eq!(max_laps(&f(3, 50), &f(11, 20), &f(13, 20)), 2);
+    assert_eq!(max_laps(&frac(3, 50), &frac(11, 20), &frac(13, 20)), 2);
 }
