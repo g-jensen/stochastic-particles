ad52386c33dff4bb225595129142a297d115d149
greg <greg.micah.jensen@gmail.com>
2026-02-19 15:51:54 -0500

    impl WIP lap_count theory

--- rust/src/static/theory/mod.rs
+++ rust/src/static/theory/mod.rs
@@ -9,3 +9,30 @@ pub fn should_lap_forever(phase: &Fraction, travel_time: &Fraction, on_ratio: &F
     let one = Fraction::one();
     (phase + (((q * (one - *phase)).ceil() - one) / q)).le(on_ratio)
 }
+
+fn mod_inverse(p: i64, q: i64) -> i64 {
+    let (mut old_r, mut r) = (p,q);
+    let (mut old_s, mut s) = (1,0);
+    let (mut old_t, mut t) = (0,1);
+
+    while r != 0 {
+        let quot = old_r / r;
+        (old_r, r) = (r, old_r - quot * r);
+        (old_s, s) = (s, old_s - quot * s);
+        (old_t, t) = (t, old_t - quot * t);
+    };
+
+    ((old_s % q) + q) % q
+}
+
+// assumes !should_lap_forever
+pub fn lap_count(phase: &Fraction, travel_time: &Fraction, on_ratio: &Fraction) -> u64 {
+    let p: u64 = (*travel_time.numer().unwrap()).try_into().unwrap();
+    let q: u64 = (*travel_time.denom().unwrap()).try_into().unwrap();
+    let p_inv: u64 = mod_inverse(p as i64, q as i64).try_into().unwrap();
+    let one = Fraction::one();
+    (0..q)
+    .filter(|k| ((phase + Fraction::new(*k, q)) % one) > *on_ratio)
+    .map(|k| (k * p_inv) % q)
+    .min().unwrap()-1
+}
--- rust/src/static/theory/test.rs
+++ rust/src/static/theory/test.rs
@@ -16,8 +16,30 @@ fn should_lap_forever_table() {
     ];
 
     for (phase, travel_time, on_ratio, expected) in cases {
-        assert_eq!(should_lap_forever(&phase, &travel_time, &on_ratio),
-        expected,
-        "phase={phase}, travel_time={travel_time}, on_ratio={on_ratio}");
+        assert_eq!(
+            expected,
+            should_lap_forever(&phase, &travel_time, &on_ratio),
+            "phase={phase}, travel_time={travel_time}, on_ratio={on_ratio}");
+    }
+}
+
+#[test]
+fn lap_count_table() {
+    let cases = [
+        (frac(1, 10),  frac(1, 2),    frac(1, 2),    0),
+        (frac(0, 1),   frac(7, 10),   frac(1, 2),    0),
+        (frac(1, 10),  frac(3, 2),    frac(1, 2),    0),
+        (frac(0, 1),   frac(3, 10),   frac(1, 2),    1),
+        (frac(0, 1),   frac(1, 4),    frac(1, 2),    2),
+        (frac(6, 100), frac(55, 100), frac(65, 100), 2),
+        (frac(1, 2),   frac(55, 100), frac(65, 100), 3),
+    ];
+
+    for (phase, travel_time, on_ratio, expected) in cases {
+        assert_eq!(
+            expected,
+            lap_count(&phase, &travel_time, &on_ratio),
+            "phase={phase}, travel_time={travel_time}, on_ratio={on_ratio}"
+        );
     }
 }
