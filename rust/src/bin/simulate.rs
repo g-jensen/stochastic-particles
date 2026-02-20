54be09ebb718aeff9051e6206740d457b102055b
greg <greg.micah.jensen@gmail.com>
2026-02-20 11:40:23 -0500

    simulate executable outputs csv

--- rust/src/bin/simulate.rs
+++ rust/src/bin/simulate.rs
@@ -10,12 +10,13 @@ fn main() -> io::Result<()> {
     let granularity = arg(&args, "-granularity", 10);
     let max_travel_time = arg(&args, "-max-travel-time", Fraction::new(1u64, 1u64));
     let max_laps = arg(&args, "-max-laps", granularity+1);
-    let output_path = arg(&args, "-output", String::from("output.txt"));
+    let output_path = arg(&args, "-output", String::from("output.csv"));
     let file = File::create(output_path)?;
     let mut writer = BufWriter::new(file);
 
     let mut count = 0u64;
 
+    writeln!(writer, "phase, travel_time, on_ratio, laps")?;
     sampling::sample(
         granularity,
         max_travel_time,
@@ -23,7 +24,7 @@ fn main() -> io::Result<()> {
             count += 1;
             let laps_opt = simulation::lap_count(&phase,&travel_time,&on_ratio,max_laps);
             let laps_str = laps_opt.and_then(|lap_count| Some(lap_count.to_string())).or(Some(String::from("inf"))).unwrap();
-            writeln!(writer, "{} {} {} {}", &phase, &travel_time, &on_ratio, laps_str).unwrap();
+            writeln!(writer, "{}, {}, {}, {}", &phase, &travel_time, &on_ratio, laps_str).unwrap();
         },
     );
 
