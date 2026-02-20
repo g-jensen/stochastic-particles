44192034cf0f42c604dd87f9b3cf8d1c0e7825b8
greg <greg.micah.jensen@gmail.com>
2026-02-20 17:40:22 -0500

    fix specs

--- rust/src/basic/sampling/test.rs
+++ rust/src/basic/sampling/test.rs
@@ -20,15 +20,13 @@ fn sample_produces_4_combinations_for_granularity_2() {
         calls,
         vec![
             (frac(0, 2), frac(1, 2), frac(1, 2)),
-            (frac(0, 2), frac(2, 2), frac(1, 2)),
             (frac(1, 2), frac(1, 2), frac(1, 2)),
-            (frac(1, 2), frac(2, 2), frac(1, 2)),
         ]
     );
 }
 
 #[test]
-fn sample_produces_18_combinations_for_granularity_3() {
+fn sample_produces_12_combinations_for_granularity_3() {
     let mut calls: Vec<(Fraction, Fraction, Fraction)> = Vec::new();
     sample(3, frac(1, 1), &mut |phase, travel_time, on_ratio| {
         calls.push((phase, travel_time, on_ratio));
@@ -38,30 +36,24 @@ fn sample_produces_18_combinations_for_granularity_3() {
         vec![
             (frac(0, 3), frac(1, 3), frac(1, 3)),
             (frac(0, 3), frac(2, 3), frac(1, 3)),
-            (frac(0, 3), frac(3, 3), frac(1, 3)),
             (frac(0, 3), frac(1, 3), frac(2, 3)),
             (frac(0, 3), frac(2, 3), frac(2, 3)),
-            (frac(0, 3), frac(3, 3), frac(2, 3)),
             
             (frac(1, 3), frac(1, 3), frac(1, 3)),
             (frac(1, 3), frac(2, 3), frac(1, 3)),
-            (frac(1, 3), frac(3, 3), frac(1, 3)),
             (frac(1, 3), frac(1, 3), frac(2, 3)),
             (frac(1, 3), frac(2, 3), frac(2, 3)),
-            (frac(1, 3), frac(3, 3), frac(2, 3)),
 
             (frac(2, 3), frac(1, 3), frac(1, 3)),
             (frac(2, 3), frac(2, 3), frac(1, 3)),
-            (frac(2, 3), frac(3, 3), frac(1, 3)),
             (frac(2, 3), frac(1, 3), frac(2, 3)),
             (frac(2, 3), frac(2, 3), frac(2, 3)),
-            (frac(2, 3), frac(3, 3), frac(2, 3)),
         ]
     );
 }
 
 #[test]
-fn sample_travel_time_has_granularity_steps() {
+fn sample_travel_time_has_granularity_minus_one_steps() {
     let mut calls: Vec<(Fraction, Fraction, Fraction)> = Vec::new();
     sample(2, frac(3, 2), &mut |phase, travel_time, on_ratio| {
         calls.push((phase, travel_time, on_ratio));
@@ -70,9 +62,7 @@ fn sample_travel_time_has_granularity_steps() {
         calls,
         vec![
             (frac(0, 2), frac(3, 4), frac(1, 2)),
-            (frac(0, 2), frac(3, 2), frac(1, 2)),
             (frac(1, 2), frac(3, 4), frac(1, 2)),
-            (frac(1, 2), frac(3, 2), frac(1, 2)),
         ]
     );
 }
