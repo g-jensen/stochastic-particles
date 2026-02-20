124787294f01224896f313c865a585c88923e100
greg <greg.micah.jensen@gmail.com>
2026-02-19 22:05:09 -0500

    theory *actually* accounts for particles starting out of sync

--- rust/src/basic/theory/mod.rs
+++ rust/src/basic/theory/mod.rs
@@ -4,10 +4,14 @@ mod test;
 use fraction::Fraction;
 use fraction::One;
 
+fn fractional_part(f: Fraction) -> Fraction {
+    f - f.floor()
+}
+
 fn should_lap_forever(phase: &Fraction, q: u64, on_ratio: &Fraction) -> bool {
     let one = Fraction::one();
     let q_frac = Fraction::from(q);
-    (phase + (((q_frac * (one - *phase)).ceil() - 1) / q_frac)).le(on_ratio)
+    fractional_part(phase + (((q_frac * (one - *phase)).ceil() - 1) / q_frac)).le(on_ratio)
 }
 
 fn mod_inverse(p: i64, q: i64) -> i64 {
@@ -26,17 +30,17 @@ fn mod_inverse(p: i64, q: i64) -> i64 {
 }
 
 pub fn lap_count(phase: &Fraction, travel_time: &Fraction, on_ratio: &Fraction) -> Option<u64> {
-    let p: u64 = (*travel_time.numer().unwrap()).try_into().unwrap();
-    let q: u64 = (*travel_time.denom().unwrap()).try_into().unwrap();
+    let p: u64 = *travel_time.numer().unwrap();
+    let q: u64 = *travel_time.denom().unwrap();
     if should_lap_forever(phase, q, on_ratio) {
         return None;
     }
     let p_inv: u64 = mod_inverse(p as i64, q as i64).try_into().unwrap();
 
-    (1..q)
-        .filter(|k| ((phase + Fraction::new(*k, q)) % 1) > *on_ratio)
+    (0..q)
+        .filter(|k| fractional_part(phase + Fraction::new(*k, q)) > *on_ratio)
         .map(|k| (k * p_inv) % q)
+        .map(|n| if n == 0 {q} else {n})
         .min()
-        .and_then(|n| Some(n - 1))
-        .or(Some(0))
+        .and_then(|n| Some(n-1))
 }
