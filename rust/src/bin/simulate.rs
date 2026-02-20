5119f511be5587bce99dba57d62825aacfbac0b9
greg <greg.micah.jensen@gmail.com>
2026-02-20 12:26:08 -0500

    simulate executable can use any strategy

--- rust/src/bin/simulate.rs
+++ rust/src/bin/simulate.rs
@@ -2,8 +2,14 @@ use std::fs::File;
 use std::io::{self, BufWriter, Write};
 
 use fraction::Fraction;
-use stochastic::basic::{sampling, simulation};
+use stochastic::basic::{sampling, simulation, theory};
 use stochastic::cli::{arg};
+
+#[derive(Debug)]
+enum Strategy {
+    THEORY,
+    SIMULATION
+}
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
@@ -11,18 +17,28 @@ fn main() -> io::Result<()> {
     let max_travel_time = arg(&args, "-max-travel-time", Fraction::new(1u64, 1u64));
     let max_laps = arg(&args, "-max-laps", granularity+1);
     let output_path = arg(&args, "-output", String::from("output.csv"));
+    let strategy_name = arg(&args, "-strategy", String::from("theory"));
+    let strategy =
+     if strategy_name.to_lowercase() == "theory" 
+        {Strategy::THEORY}
+        else {Strategy::SIMULATION};
     let file = File::create(output_path)?;
     let mut writer = BufWriter::new(file);
 
     let mut count = 0u64;
 
+    println!("Using strategy: {:#?}",strategy);
     writeln!(writer, "phase, travel_time, on_ratio, laps")?;
     sampling::sample(
         granularity,
         max_travel_time,
         &mut |phase, travel_time, on_ratio| {
             count += 1;
-            let laps_opt = simulation::lap_count(&phase,&travel_time,&on_ratio,max_laps);
+            let laps_opt;
+            match strategy {
+                Strategy::THEORY => laps_opt = theory::lap_count(&phase,&travel_time,&on_ratio),
+                Strategy::SIMULATION => laps_opt = simulation::lap_count(&phase,&travel_time,&on_ratio,max_laps),
+            }
             let laps_str = laps_opt.and_then(|lap_count| Some(lap_count.to_string())).or(Some(String::from("inf"))).unwrap();
             writeln!(writer, "{}, {}, {}, {}", &phase, &travel_time, &on_ratio, laps_str).unwrap();
         },
