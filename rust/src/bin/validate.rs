82e763a5369585109d61611d8bf84b0ad91fec1e
greg <greg.micah.jensen@gmail.com>
2026-02-19 22:30:33 -0500

    default max-travel-time is 1

--- rust/src/bin/validate.rs
+++ rust/src/bin/validate.rs
@@ -5,7 +5,7 @@ use stochastic::cli::{arg_fraction, arg_uint};
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let granularity = arg_uint(&args, "-granularity", 10);
-    let max_travel_time = arg_fraction(&args, "-max-travel-time", Fraction::new(3u64, 1u64));
+    let max_travel_time = arg_fraction(&args, "-max-travel-time", Fraction::new(1u64, 1u64));
     let max_laps = arg_uint(&args, "-max-laps", granularity+1);
 
     let mut count = 0u64;
