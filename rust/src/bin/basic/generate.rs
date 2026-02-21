122ba55a36174732fdbbcd7e7c8d962ee3a1b364
greg <greg.micah.jensen@gmail.com>
2026-02-20 22:02:23 -0500

    generate uses lifespan optimization

--- rust/src/bin/basic/generate.rs
+++ rust/src/bin/basic/generate.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::{self, BufWriter, Write};
 
-use fraction::Fraction;
-use stochastic::basic::{sampling, simulation, theory};
+use fraction::{ConstOne, Fraction};
+use stochastic::basic::{lifespan, sampling, simulation, theory};
 use stochastic::cli::{arg};
 
 #[derive(Debug)]
@@ -14,7 +14,7 @@ enum Strategy {
 fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let granularity = arg(&args, "-granularity", 10);
-    let max_travel_time = arg(&args, "-max-travel-time", Fraction::new(1u64, 1u64));
+    let max_travel_time = arg(&args, "-max-travel-time", Fraction::ONE);
     let max_laps = arg(&args, "-max-laps", granularity+1);
     let output_path = arg(&args, "-output", String::from("output.csv"));
     let strategy_name = arg(&args, "-strategy", String::from("theory"));
@@ -22,28 +22,29 @@ fn main() -> io::Result<()> {
      if strategy_name.to_lowercase() == "theory" 
         {Strategy::THEORY}
         else {Strategy::SIMULATION};
+    let simulation_lap_fn: &dyn Fn(&Fraction, &Fraction, &Fraction) -> Option<u64> = 
+    &|p,t,o| {
+        simulation::lap_count(p,t,o,max_laps)
+    };
     let file = File::create(output_path)?;
     let mut writer = BufWriter::new(file);
 
     let mut count = 0u64;
 
     println!("Using strategy: {:#?}",strategy);
-    writeln!(writer, "phase, on_ratio, travel_time, lifespan")?;
-    sampling::sample_all(
-        granularity,
-        max_travel_time,
-        &mut |phase, travel_time, on_ratio| {
-            count += 1;
-            let laps_opt;
+    writeln!(writer, "phase, on_ratio, optimal_travel_time, optimal_lifespan")?;
+    sampling::sample_phase(granularity, &mut |phase| {
+        sampling::sample_on_ratio(granularity, &mut |on_ratio| {
+            let lap_fn: &dyn Fn(&Fraction, &Fraction, &Fraction) -> Option<u64>;
             match strategy {
-                Strategy::THEORY => laps_opt = theory::lap_count(&phase,&travel_time,&on_ratio),
-                Strategy::SIMULATION => laps_opt = simulation::lap_count(&phase,&travel_time,&on_ratio,max_laps),
+                Strategy::THEORY => lap_fn = &theory::lap_count,
+                Strategy::SIMULATION => lap_fn = simulation_lap_fn,
             }
-            let lifespan_opt = laps_opt.and_then(|laps| Some(Fraction::from(laps+1) * travel_time));
-            let lifespan_str = lifespan_opt.and_then(|lifespan| Some(lifespan.to_string())).or(Some(String::from("inf"))).unwrap();
-            writeln!(writer, "{}, {}, {}, {}", &phase, &on_ratio, &travel_time, lifespan_str).unwrap();
-        },
-    );
+            let (travel_time, lifespan) = lifespan::optimize(phase, on_ratio, granularity, max_travel_time, lap_fn);
+            writeln!(writer, "{}, {}, {}, {}", &phase, &on_ratio, &travel_time, lifespan.to_string()).unwrap();
+            count += 1;
+        });
+    });
 
     println!("Simulated {count} input combinations.");
     Ok(())
